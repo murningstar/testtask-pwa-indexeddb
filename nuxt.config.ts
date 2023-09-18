79b865fe48d9670cdb0224257003e80dc9bdf513
menyadeniskostin <menyadeniskostin@gmail.com>
2023-09-18 12:06:48 +0300

    nuxt&vite config adjustment bruteforce#7 fix icon paths

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import sanitizer from "sanitize-filename";
+const repoPath = "/testtask-pwa-indexeddb";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     // devtools: { enabled: true }
@@ -23,27 +24,27 @@ export default defineNuxtConfig({
             background_color: "#89bbde",
             icons: [
                 {
-                    src: "/icons/pwa-64x64.png",
+                    src: repoPath + "/icons/pwa-64x64.png",
                     sizes: "64x64",
                     type: "image/png",
                 },
                 {
-                    src: "/icons/apple-touch-icon-180x180.png",
+                    src: repoPath + "/icons/apple-touch-icon-180x180.png",
                     sizes: "180x180",
                     type: "image/png",
                 },
                 {
-                    src: "/icons/pwa-192x192.png",
+                    src: repoPath + "/icons/pwa-192x192.png",
                     sizes: "192x192",
                     type: "image/png",
                 },
                 {
-                    src: "/icons/pwa-512x512.png",
+                    src: repoPath + "/icons/pwa-512x512.png",
                     sizes: "512x512",
                     type: "image/png",
                 },
                 {
-                    src: "/icons/maskable-icon-512x512.png",
+                    src: repoPath + "/icons/maskable-icon-512x512.png",
                     sizes: "512x512",
                     type: "image/png",
                     purpose: "maskable",
