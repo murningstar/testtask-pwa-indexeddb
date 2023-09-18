47726c7e00da63066e4e69686db6eed27d49c045
menyadeniskostin <menyadeniskostin@gmail.com>
2023-09-18 13:16:02 +0300

    NuxtConfig split into $development and $production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,81 +1,63 @@
 import sanitizer from "sanitize-filename";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    // devtools: { enabled: true }
-    ssr: false,
-    app: {
-        baseURL: "/testtask-pwa-indexeddb/",
-        buildAssetsDir: "assets",
-    },
-    css: ["/assets/scss/main.scss"],
-
-    modules: ["@vite-pwa/nuxt"],
-
-    pwa: {
-        base: "/testtask-pwa-indexeddb/",
-
-        manifest: {
-            name: "Noviy Disk",
-            short_name: "Noviy Disk",
-            start_url: "/testtask-pwa-indexeddb/",
-            scope: "/testtask-pwa-indexeddb/",
-            theme_color: "#f7f583",
-            background_color: "#89bbde",
-            icons: [
-                {
-                    src: "/testtask-pwa-indexeddb/icons/pwa-64x64.png",
-                    sizes: "64x64",
-                    type: "image/png",
-                },
-                {
-                    src: "/testtask-pwa-indexeddb/icons/apple-touch-icon-180x180.png",
-                    sizes: "180x180",
-                    type: "image/png",
-                },
-                {
-                    src: "/testtask-pwa-indexeddb/icons/pwa-192x192.png",
-                    sizes: "192x192",
-                    type: "image/png",
-                },
-                {
-                    src: "/testtask-pwa-indexeddb/icons/pwa-512x512.png",
-                    sizes: "512x512",
-                    type: "image/png",
-                },
-                {
-                    src: "/testtask-pwa-indexeddb/icons/maskable-icon-512x512.png",
-                    sizes: "512x512",
-                    type: "image/png",
-                    purpose: "maskable",
-                },
-            ],
+    $production: {
+        ssr: false,
+        app: {
+            baseURL: "/testtask-pwa-indexeddb/",
+            buildAssetsDir: "assets",
         },
-        /* mode: "development",
-        registerType: "autoUpdate", */
+        css: ["/assets/scss/main.scss"],
 
-        strategies: "generateSW",
-        // srcDir: "src",
-        // filename: "sw.js",
-        workbox: {
-            cleanupOutdatedCaches: true,
-            globPatterns: ["**/*.{js,css,html,ico,png}"],
+        modules: ["@vite-pwa/nuxt"],
 
-            /* runtimeCaching:[     // Используется с generatesw-стратегией для кэша динамических штук. Для статики используется globPatterns
-                {
-                    handler: "StaleWhileRevalidate",
-                    urlPattern:  
-                }
-            ], */
+        pwa: {
+            base: "/testtask-pwa-indexeddb/",
+
+            manifest: {
+                name: "Noviy Disk",
+                short_name: "Noviy Disk",
+                start_url: "/testtask-pwa-indexeddb/",
+                scope: "/testtask-pwa-indexeddb/",
+                theme_color: "#f7f583",
+                background_color: "#89bbde",
+                icons: [
+                    {
+                        src: "/testtask-pwa-indexeddb/icons/pwa-64x64.png",
+                        sizes: "64x64",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/testtask-pwa-indexeddb/icons/apple-touch-icon-180x180.png",
+                        sizes: "180x180",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/testtask-pwa-indexeddb/icons/pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/testtask-pwa-indexeddb/icons/pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/testtask-pwa-indexeddb/icons/maskable-icon-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+            },
+            strategies: "generateSW",
+            workbox: {
+                cleanupOutdatedCaches: true,
+                globPatterns: ["**/*.{js,css,html,ico,png}"],
+            },
+            includeAssets: ["index.html"],
         },
-        // includeAssets: ["index.html"],
-        // devOptions: { enabled: true, type: "module" },
-    },
-
-    devtools: {
-        // enabled: false,
-    },
-
-    /* build: {
+        /* build: {
         transpile:
             process.env.NODE_ENV === "production"
                 ? [
@@ -87,30 +69,99 @@ export default defineNuxtConfig({
                   ]
                 : ["@juggle/resize-observer"],
     }, */
-    vite: {
-        base: "/testtask-pwa-indexeddb/",
+        vite: {
+            base: "/testtask-pwa-indexeddb/",
 
-        build: {
-            rollupOptions: {
-                output: {
-                    sanitizeFileName: (string) => {
-                        let sanitized = sanitizer(string);
-                        if (sanitized[0] === "_") {
-                            sanitized = sanitized.slice(1, sanitized.length);
-                        } else if (sanitized[1] === "_") {
-                            sanitized = sanitized.slice(1, sanitized.length);
-                        }
-                        return sanitized;
+            build: {
+                rollupOptions: {
+                    output: {
+                        sanitizeFileName: (string) => {
+                            let sanitized = sanitizer(string);
+                            if (sanitized[0] === "_") {
+                                sanitized = sanitized.slice(
+                                    1,
+                                    sanitized.length
+                                );
+                            } else if (sanitized[1] === "_") {
+                                sanitized = sanitized.slice(
+                                    1,
+                                    sanitized.length
+                                );
+                            }
+                            return sanitized;
+                        },
                     },
                 },
+                assetsInlineLimit: 0,
             },
-            assetsInlineLimit: 0,
         },
-        /* optimizeDeps: {
+    },
+    $development: {
+        devtools: { enabled: true },
+        css: ["/assets/scss/main.scss"],
+        modules: ["@vite-pwa/nuxt"],
+        pwa: {
+            manifest: {
+                name: "Noviy Disk",
+                short_name: "Noviy Disk",
+                theme_color: "#f7f583",
+                background_color: "#89bbde",
+                icons: [
+                    {
+                        src: "/icons/pwa-64x64.png",
+                        sizes: "64x64",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icons/apple-touch-icon-180x180.png",
+                        sizes: "180x180",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icons/pwa-192x192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icons/pwa-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                    {
+                        src: "/icons/maskable-icon-512x512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        purpose: "maskable",
+                    },
+                ],
+            },
+            // mode: "development",
+            // registerType: "autoUpdate",
+
+            strategies: "generateSW",
+            // srcDir: "src",
+            // filename: "sw.js",
+            workbox: {
+                cleanupOutdatedCaches: true,
+                globPatterns: ["**/*.{js,css,html,ico,png}"],
+
+                /* runtimeCaching:[     // Используется с generatesw-стратегией для кэша динамических штук. Для статики используется globPatterns
+                {
+                    handler: "StaleWhileRevalidate",
+                    urlPattern:  
+                }
+            ], */
+            },
+            // includeAssets: ["index.html"],
+            // devOptions: { enabled: true, type: "module" },
+        },
+        vite: {
+            /* optimizeDeps: {
             include:
                 process.env.NODE_ENV === "development"
                     ? ["naive-ui", "vueuc", "date-fns-tz/esm/formatInTimeZone"]
                     : [],
         }, */
+        },
     },
 });
